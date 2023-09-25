947d16613a0dab6115eeaada8a3bce273e2bafac
RexshackGaming <rexshackuk@gmail.com>
2023-09-25 10:33:43 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'dev-coords'
+
+client_scripts {
+	"coords.lua"
+}
+
+ui_page 'nui/index.html'
+
+files {
+	'nui/app.js',
+	'nui/index.html',
+	'nui/image/logo.png',
+	'nui/style.css',
+}
